760184157f43b436ef1cf684c48d1e63c0d8c6e2
mani-2012 <106404786+mani-2012@users.noreply.github.com>
2022-12-23 17:45:43 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,22 +1,36 @@
 function setup() {
-    canvas=createCanvas(400,400);
+  cuatro=createCanvas(350,350);
+ background("white");
+  cuatro.position(500,250);
+  cuatro.mouseReleased(noche);
+  }
+   function noche(){
+     pet.classify(cuatro,felices);
+   }
+   function felices(error,resultado){
+     if (error) {
+       console.error(error);
+     }
+     else {
+       console.log(resultado);
+       document.getElementById("label").innerHTML="Etiqueta: "+resultado[0].label;
+       document.getElementById("confidence").innerHTML="Confianza: "+resultado[0].confidence;
+     }
+ 
+   }
+ 
+ function preload() {
+   pet=ml5.imageClassifier('DoodleNet');
+ }
+   
+ 
+ function draw() {
+  strokeWeight(14);
+  stroke(0);
+  if (mouseIsPressed){
+  line(pmouseX,pmouseY,mouseX,mouseY);
+  }
+ } 
+ function regalo(){
    background("white");
-    canvas.position(500,250);
-   }
-     
-   
-   function preload() {
-     
-   }
-     
-   
-   function draw() {
-    strokeWeight(15);
-    stroke(0);
-    if (mouseIsPressed){
-    line(pmouseX,pmouseY,mouseX,mouseY);
-    }
-   } 
-   function regalo(){
-     background("white");
-   }
+ }
